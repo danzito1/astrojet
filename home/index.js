573f83afe7748119b7b6a2ac2e42f503c648751c
danzito1 <108548505+danzito1@users.noreply.github.com>
2023-04-26 14:45:44 -0300

    Update index.js

--- home/index.js
+++ home/index.js
@@ -1,1 +1,8 @@
+const flights = document.getElementById("list_flights");
+flights.addEventListener("click", function(event) {
+    if (event.target.tagName === "LI" && event.target.parentElement === flights) {
+        alert("O item que foi clicado: " + event.target.textContent);
+    }
+});
+
 
