aeb0dcacfcc0c98c7c5ca6ce0ff868bff3c24ed1
danzito1 <108548505+danzito1@users.noreply.github.com>
2023-04-29 18:25:28 -0300

    Update viewflight.js

--- home/viewflight.js
+++ home/viewflight.js
@@ -11,9 +11,16 @@ window.onload = function() {
     if (aviao === "AT76") {
         let num = Math.floor(Math.random() * 2) + 1;
         if (num == 1) {
-            img.setAttribute("src", "");
+            img.setAttribute("src", "270218379_1611388632540815_550111631583611342_n.jpg");
         } else if (num == 2) {
-            img.setAttribute("src", "");
+            img.setAttribute("src", "271072038_1256185188207498_589227468577376344_n.jpg");
+        }
+    } else if (aviao === "A223") {
+        let num = Math.floor(Math.random() * 3) + 1;
+        if (num == 1) {
+            img.setAttribute("src", "270218379_1611388632540815_550111631583611342_n.jpg");
+        } else if (num == 2) {
+            img.setAttribute("src", "271072038_1256185188207498_589227468577376344_n.jpg");
         }
     }
     
