80aaa94fc3b68f0a2ce785495009c0abcafe46f4
danzito1 <108548505+danzito1@users.noreply.github.com>
2023-04-29 18:19:15 -0300

    Update viewflight.js

--- home/viewflight.js
+++ home/viewflight.js
@@ -1,6 +1,15 @@
 window.onload = function() {
+    
+    var img = document.getElementById('banner');
     const infos = JSON.parse(localStorage.getItem('flightClicked'));
     if (infos != null && infos[2] != null) {
+
         document.title = "Vôo " + infos[2].split("/")[0];
     }
+    
+    var aviao = infos[2].split("/")[1];
+    if (aviao === "AT76") {
+        for 
+    }
+    
 }
