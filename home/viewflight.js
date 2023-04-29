0cde774d3a86ae721502b38c562123c6eee0a547
danzito1 <108548505+danzito1@users.noreply.github.com>
2023-04-29 16:17:43 -0300

    Update viewflight.js

--- home/viewflight.js
+++ home/viewflight.js
@@ -1,6 +1,6 @@
 window.onload = function() {
     const infos = JSON.parse(localStorage.getItem('flightClicked'));
     if (infos != null && infos[2] != null) {
-        document.title = "Observando o vôo " + infos[2].split("/")[0];
+        document.title = "Vôo " + infos[2].split("/")[0];
     }
 }
