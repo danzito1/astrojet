1b82e214a6bd9facf9b314dd4afad9af5c8a07bb
danzito1 <108548505+danzito1@users.noreply.github.com>
2023-04-29 08:42:41 -0300

    Update index.js

--- home/index.js
+++ home/index.js
@@ -3,6 +3,7 @@ const item = flights.getElementByTagName('li');
 
 for (let i = 0; i < item.length; i++) {
     item[i].addEventListener('click', function() {
+        alert('o item clicado foi: ' + String(i));
         localStorage.setItem('flightClicked', i.toString());
         location.href = 'viewflight.html';
     });
