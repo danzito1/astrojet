040c7b76fb9a932f97c6d74564d54c688dc8c014
danzito1 <108548505+danzito1@users.noreply.github.com>
2023-04-29 15:44:22 -0300

    Create viewflight.js

---
+++ home/viewflight.js
@@ -0,0 +1,6 @@
+window.onload = function() {
+            const infos = JSON.parse(localStorage.getItem('flightClicked'));
+            if (infos != null && infos[2] != null) {
+                document.title = "Observando o vôo " + infos[2].split("•")[0].replace(" ", "");
+            }
+        }
